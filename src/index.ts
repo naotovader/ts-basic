702174831aa503fa0d33faa7ae4f62aa3ca3d50d
naotovader <gallardo.st62@gmail.com>
2021-08-12 11:47:38 +0900

    #5 関数への型付与

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,23 @@
 // world.sayHello(root);
 
 // 03.基本の型定義
-import { primitiveSample, notExistSample, anySample, unknownSample } from "./basic";
+// import { primitiveSample, notExistSample, anySample, unknownSample } from "./basic";
 
-unknownSample()
-anySample()
-notExistSample()
-primitiveSample()
+// unknownSample()
+// anySample()
+// notExistSample()
+// primitiveSample()
+
+// 04.関数の型定義
+import { logMessage, logMessage2, logMessage3, logMessage4 } from "./function/basic";
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from "./function/parameters"
+
+logMessage('Hello TypeScript!')
+logMessage2('Hello TypeScript!')
+logMessage3('Hello TypeScript!')
+logMessage4('Hello TypeScript!')
+
+isUserSignedIn('AC')
+isUserSignedIn2('ABC')
+const sum: number = sumProductsPrice(10,20,30)
+console.log('Function parameter sample 5:', sum)
