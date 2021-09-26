0e9fd71e932c9f0e5682920df99dd584c98f9e29
naotovader <gallardo.st62@gmail.com>
2021-09-26 13:21:20 +0900

    #10 非同期処理

---
+++ src/asynchronous/callback.ts
@@ -0,0 +1,25 @@
+export default function callbackSample() {
+  const url = "https://api.github.com/users/naotovader";
+
+  const fetchProfile = () => {
+    return fetch(url)
+      .then((res) => {
+        // レスポンスのBodyをJSONで読み取った結果を返す
+        res
+          .json()
+          .then((json) => {
+            console.log("Asynchronous Callback Sample 1:", json);
+            return json;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const profile = fetchProfile();
+  console.log("Asynchronous Callback Sample 2:", profile);
+}
